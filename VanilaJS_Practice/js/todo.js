dbb9f30aebd62a8720960039033ca6692773d996
yunseo323 <jokani3244833@gmail.com>
2021-07-28 23:44:59 +0900

    add-delete

--- VanilaJS_Practice/js/todo.js
+++ VanilaJS_Practice/js/todo.js
@@ -1,11 +1,26 @@
-const todoForm = document.getElementById("todo-form");
-const todoInput = todoForm.querySelector("input");
-const todoList = document.getElementById("todo-list");
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
 
+
+function paintToDo(newToDo){
+    const li = document.createElement("li");
+    const span=document.createElement("span");
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText="❌"
+    li.appendChild(span);
+    li.appendChild(deleteButton);
+    span.innerText=newToDo;
+    toDoList.appendChild(li);
+    
+}
 
 function handleToDoSubmit(event){
-    
+  event.preventDefault();
+  const newToDo = toDoInput.value;
+  toDoInput.value=""; //비우기
+  paintToDo(newToDo);
 };
 
 
-todoForm.addEventListener("submit",handleToDoSubmit);
+toDoForm.addEventListener("submit",handleToDoSubmit);
