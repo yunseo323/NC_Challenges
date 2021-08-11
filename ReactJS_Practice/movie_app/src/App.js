30aa7c87d52609b84a0b50784232a9f1d2f3ea4a
yunseo323 <jokani3244833@gmail.com>
2021-08-11 23:56:25 +0900

    11

---
+++ ReactJS_Practice/movie_app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class App extends React.Component{
+    state = {
+        isLoading: true
+    }
+    render(){
+        const {isLoading} = this.state;
+        return(
+            <div>
+                {isLoading? "Loading":"We're Ready"}
+            </div>
+        )
+    }
+}
+
+export default App;
